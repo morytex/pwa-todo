5e8b8588096f01ea33b4158b3b3b789eeb682906
Alessandro Moryta Suemasu <moryta@ciandt.com>
2020-01-15 08:39:09 -0300

    Renaming project

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# PWATodo
+# pwa-todo
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.22.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'PWATodo'`, () => {
+  it(`should have as title 'pwa-todo'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('PWATodo');
+    expect(app.title).toEqual('pwa-todo');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('PWATodo app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('pwa-todo app is running!');
   });
 });
